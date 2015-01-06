3e93d80814f2256e3412d3cb21a1e1729785925d
Brian Brotsos <bbrotsos@gmail.com>
2015-01-05 22:22:33 -0500

    Testing source formatting on java.

--- src/main/java/gov/usda/DataCatalogClient/Publisher.java
+++ src/main/java/gov/usda/DataCatalogClient/Publisher.java
@@ -3,9 +3,11 @@ package gov.usda.DataCatalogClient;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.json.simple.JSONObject;
+
 /**
- * The Publisher class is based on Project Open Data metadata specification 1.1.  Details can
- * be found here https://project-open-data.cio.gov/v1.1/schema/#publisher
+ * The Publisher class is based on Project Open Data metadata specification 1.1.
+ * Details can be found here
+ * https://project-open-data.cio.gov/v1.1/schema/#publisher
  * 
  * @author bbrotsos
  *
@@ -15,7 +17,7 @@ public class Publisher {
 	public final static String PROJECT_OPEN_DATA_PUBLISHER_NAME = "name";
 	public final static String PROJECT_OPEN_DATA_PUBLISHER_SUBORGANIZATION = "subOrganizationOf";
 	public final static String PROJECT_OPEN_DATA_PUBLISHER_TYPE = "@type";
-	
+
 	public final static String CKAN_PUBLISHER = "publisher";
 	public final static String CKAN_PUBLISHER_NAME = "publisher";
 	public final static String CKAN_PUBLISHER_SUBORGANIZATION_NAME = "publisher_1";
@@ -27,101 +29,95 @@ public class Publisher {
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Publisher getSubOrganization() {
 		return subOrganization;
 	}
+
 	public void setSubOrganization(Publisher subOrganization) {
 		this.subOrganization = subOrganization;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public JSONObject toProjectOpenDataJSON()
-	{
+	public JSONObject toProjectOpenDataJSON() {
 		JSONObject publisherMap = new JSONObject();
 		publisherMap.put(PROJECT_OPEN_DATA_PUBLISHER_NAME, name);
-		if (subOrganization != null)
-		{
-			publisherMap.put(PROJECT_OPEN_DATA_PUBLISHER_SUBORGANIZATION, subOrganization.toProjectOpenDataJSON());
+		if (subOrganization != null) {
+			publisherMap.put(PROJECT_OPEN_DATA_PUBLISHER_SUBORGANIZATION,
+					subOrganization.toProjectOpenDataJSON());
 		}
 		publisherMap.put(PROJECT_OPEN_DATA_PUBLISHER_TYPE, type);
-		
+
 		return publisherMap;
 	}
-	
-	public void loadDatasetFromPOD_JSON(JSONObject publisherProjectOpenDataJSON) throws PublisherException
-	{
-		if (publisherProjectOpenDataJSON == null)
-		{
+
+	public void loadDatasetFromPOD_JSON(JSONObject publisherProjectOpenDataJSON)
+			throws PublisherException {
+		if (publisherProjectOpenDataJSON == null) {
 			throw new PublisherException("Publisher cannot be null");
 		}
-		setName((String) publisherProjectOpenDataJSON.get(PROJECT_OPEN_DATA_PUBLISHER_NAME));
+		setName((String) publisherProjectOpenDataJSON
+				.get(PROJECT_OPEN_DATA_PUBLISHER_NAME));
 		JSONObject subOrganizationJSON = new JSONObject();
-		subOrganizationJSON = (JSONObject) publisherProjectOpenDataJSON.get(PROJECT_OPEN_DATA_PUBLISHER_SUBORGANIZATION);
-		if (subOrganizationJSON != null)
-		{
+		subOrganizationJSON = (JSONObject) publisherProjectOpenDataJSON
+				.get(PROJECT_OPEN_DATA_PUBLISHER_SUBORGANIZATION);
+		if (subOrganizationJSON != null) {
 			subOrganization = new Publisher();
 			subOrganization.loadDatasetFromPOD_JSON(subOrganizationJSON);
 		}
-		setType((String) publisherProjectOpenDataJSON.get(PROJECT_OPEN_DATA_PUBLISHER_TYPE));
-		
+		setType((String) publisherProjectOpenDataJSON
+				.get(PROJECT_OPEN_DATA_PUBLISHER_TYPE));
+
 		validatePublisher();
-		
+
 	}
-	
-	public Boolean validatePublisher() throws PublisherException
-	{
+
+	public Boolean validatePublisher() throws PublisherException {
 		Boolean validIndicator = true;
-		if (name == null)
-		{
+		if (name == null) {
 			validIndicator = false;
 			throw (new PublisherException("Publisher invalid: Name is required"));
 		}
 		return validIndicator;
 	}
-	
+
 	@Override
-	public boolean equals(Object o)
-	{
-		if (this == o)
-		{
+	public boolean equals(Object o) {
+		if (this == o) {
 			return true;
 		}
-		if (!(o instanceof Publisher))
-		{
+		if (!(o instanceof Publisher)) {
 			return false;
 		}
-		Publisher publisher_other = (Publisher)o;
-		
-		return new EqualsBuilder()
-         .append(name, publisher_other.name)
-         .append(subOrganization, publisher_other.subOrganization)
-         .append(type, publisher_other.type)
-         .isEquals();
+		Publisher publisher_other = (Publisher) o;
+
+		return new EqualsBuilder().append(name, publisher_other.name)
+				.append(subOrganization, publisher_other.subOrganization)
+				.append(type, publisher_other.type).isEquals();
 	}
-	
+
 	@Override
-	public int hashCode()
-	{
-		return new HashCodeBuilder(19, 27).
-				append(name).
-				append(subOrganization).
-				append(type).
-				toHashCode();
+	public int hashCode() {
+		return new HashCodeBuilder(19, 27).append(name).append(subOrganization)
+				.append(type).toHashCode();
 	}
+
 	@Override
 	public String toString() {
 		return "Publisher [type=" + type + ", name=" + name
 				+ ", subOrganization=" + subOrganization + "]";
 	}
-	
-	
+
 }
--- src/test/java/gov/usda/DataCatalogClient/PublisherTest.java
+++ src/test/java/gov/usda/DataCatalogClient/PublisherTest.java
@@ -13,7 +13,8 @@ import org.junit.Test;
 
 public class PublisherTest {
 
-	private static final Logger log = Logger.getLogger(PublisherTest.class.getName());
+	private static final Logger log = Logger.getLogger(PublisherTest.class
+			.getName());
 
 	@Before
 	public void setUp() throws Exception {
@@ -26,98 +27,77 @@ public class PublisherTest {
 	/**
 	 * Test the load specifically the recursive suborganization load.
 	 * 
-	 * {
-			"@type": "org:Organization",
-			"name": "Widget Services",
-			"subOrganizationOf": 
-			{
-				"@type": "org:Organization",
-				"name": "Office of Citizen Services and Innovative Technologies",
-				"subOrganizationOf": 
-				{
-					"@type": "org:Organization",
-					"name": "General Services Administration",
-					"subOrganizationOf": 
-					{
-						"@type": "org:Organization",
-						"name": "U.S. Government"
-					}
-				}
-			}
-		}	
+	 * { "@type": "org:Organization", "name": "Widget Services",
+	 * "subOrganizationOf": { "@type": "org:Organization", "name":
+	 * "Office of Citizen Services and Innovative Technologies",
+	 * "subOrganizationOf": { "@type": "org:Organization", "name":
+	 * "General Services Administration", "subOrganizationOf": { "@type":
+	 * "org:Organization", "name": "U.S. Government" } } } }
 	 */
 	@Test
 	public void testLoadPublisher() {
 		Publisher publisher = new Publisher();
 		Publisher publisherCompare = new Publisher();
 		JSONObject publisherObject = new JSONObject();
-		
+
 		String publisherTestFile = "sample_data/test/publisherTest.json";
-		try{
+		try {
 			publisherObject = Utils.loadJsonObjectFile(publisherTestFile);
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			log.log(Level.SEVERE, e.toString());
 		}
-		try
-		{
+		try {
 			publisher.loadDatasetFromPOD_JSON(publisherObject);
-		}
-		catch(PublisherException e)
-		{
+		} catch (PublisherException e) {
 			log.log(Level.SEVERE, e.toString());
 		}
-		
+
 		Publisher subOrganization4 = new Publisher();
 		subOrganization4.setName("U.S. Government");
 		subOrganization4.setType("org:Organization");
-		
+
 		Publisher subOrganization3 = new Publisher();
 		subOrganization3.setName("General Services Administration");
 		subOrganization3.setType("org:Organization");
 		subOrganization3.setSubOrganization(subOrganization4);
-		
+
 		Publisher subOrganization2 = new Publisher();
-		subOrganization2.setName("Office of Citizen Services and Innovative Technologies");
+		subOrganization2
+				.setName("Office of Citizen Services and Innovative Technologies");
 		subOrganization2.setType("org:Organization");
 		subOrganization2.setSubOrganization(subOrganization3);
-		
+
 		publisherCompare.setType("org:Organization");
 		publisherCompare.setName("Widget Services");
 		publisherCompare.setSubOrganization(subOrganization2);
-		
+
 		assertEquals(publisher, publisherCompare);
 	}
-	
+
 	@Test
 	public void testRequiredPublisher() {
 		Publisher publisher = new Publisher();
 		JSONObject publisherObject = new JSONObject();
 		PublisherException publisherException = null;
-		
+
 		String publisherRequiredTestFile = "sample_data/test/publisherRequiredTest.json";
-		try{
-			publisherObject = Utils.loadJsonObjectFile(publisherRequiredTestFile);
-		}
-		catch(Exception e)
-		{
+		try {
+			publisherObject = Utils
+					.loadJsonObjectFile(publisherRequiredTestFile);
+		} catch (Exception e) {
 			log.log(Level.SEVERE, e.toString());
 		}
-		try
-		{
+		try {
 			publisher.loadDatasetFromPOD_JSON(publisherObject);
-		}
-		catch(PublisherException e)
-		{
+		} catch (PublisherException e) {
 			publisherException = new PublisherException();
 			publisherException = e;
 			log.log(Level.SEVERE, e.toString());
 			assertEquals(e.toString(), "Publisher invalid: Name is required");
 		}
-		
+
 		assertNotNull(publisherException);
-		
+
 	}
-	
+
 }
