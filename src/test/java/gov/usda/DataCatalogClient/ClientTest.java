33fa326a37f8df101cce562897361955807035d6
Brian Brotsos <bbrotsos@gmail.com>
2018-01-31 08:55:44 -0500

    Creating forest service only branch

--- src/test/java/gov/usda/DataCatalogClient/ClientTest.java
+++ src/test/java/gov/usda/DataCatalogClient/ClientTest.java
@@ -33,10 +33,9 @@ public class ClientTest {
     	
     	try{
     		
-    		catalog_fs = odpClient.getProjectOpenDataFromURL("https://enterprisecontent-usfs.opendata.arcgis.com/data.json", "input/fs.json");
-    		catalog_ag_data_commons = odpClient.getProjectOpenDataFromURL("https://data.nal.usda.gov/data-ars.json", "input/ag-data-commons.json");
-    		try{
-    			
+    		catalog = odpClient.getProjectOpenDataFromURL("https://enterprisecontent-usfs.opendata.arcgis.com/data.json", "input/fs.json");
+    		//catalog_ag_data_commons = odpClient.getProjectOpenDataFromURL("https://data.nal.usda.gov/data-ars.json", "input/ag-data-commons.json");
+    		
     			/* 
     			 * If the Ag Data Commons times out, download fresh copy, uncomment the following line 
     			 * and comment line above that downloads from data.nal.usda.gov file.
@@ -49,16 +48,12 @@ public class ClientTest {
     			 */
     			//catalog.loadFromProjectOpenDataJSON(Utils.loadJsonObjectFile("input/fs.json"));
     			
-    			catalog_fs.hardcodeBureauCodeProgramCode();
+    			catalog.hardcodeBureauCodeProgramCode();
 
-    			catalog.loadFromProjectOpenDataJSON(Utils.loadJsonObjectFile("input/data-2018-01-31.json"));
-    			catalog.addFromOtherCatalog(catalog_ag_data_commons);
-    			catalog.addFromOtherCatalog(catalog_fs);
-    		}
-    		catch(ParseException e)
-    		{
-    			log.log(Level.SEVERE, e.toString());
-    		}
+    			//catalog.loadFromProjectOpenDataJSON(Utils.loadJsonObjectFile("input/data-2018-01-31.json"));
+    			//catalog.addFromOtherCatalog(catalog_ag_data_commons);
+    			//catalog.addFromOtherCatalog(catalog_fs);
+    		
     		
     		catalog.toCSV("output/catalog_full.csv", Catalog.DataListingCode.PUBLIC_DATA_LISTING);
     		catalog.toProjectOpenDataJSON("output/data.json", Catalog.DataListingCode.ENTERPRISE_DATA_INVENTORY);
